00ad01ff6338e12913a22b75210ba16f52a54e6b
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-08-10 21:43:15 -0700

    Implement request module

--- fetcher.js
+++ fetcher.js
@@ -1,1 +1,10 @@
+const request = require('request');
+const fs = require('fs');
 const [url, path] = process.argv.slice(2);
+
+request(url, (error, response, body) => {
+  fs.writeFile(path, body, (err) => {
+    if (err) throw err;
+    console.log(`Downloaded and saved 1235 bytes to ${path}`);
+  })
+});
